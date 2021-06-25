6605f8e5b92161a2db4e5300bd5d67a4c2a79ab1
Artur Romão <artur.romao@ua.pt>
2021-06-25 22:57:08 +0100

    propostas 100%

---
+++ D8 Imobiliarias/D8 Imobiliarias/VerPropostas.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class VerPropostas
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PropostasListBox = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.AceitarButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'PropostasListBox
+        '
+        Me.PropostasListBox.FormattingEnabled = True
+        Me.PropostasListBox.ItemHeight = 20
+        Me.PropostasListBox.Location = New System.Drawing.Point(115, 77)
+        Me.PropostasListBox.Name = "PropostasListBox"
+        Me.PropostasListBox.Size = New System.Drawing.Size(480, 324)
+        Me.PropostasListBox.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 44)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(212, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "As propostas aos seus imóveis:"
+        '
+        'AceitarButton
+        '
+        Me.AceitarButton.Location = New System.Drawing.Point(639, 358)
+        Me.AceitarButton.Name = "AceitarButton"
+        Me.AceitarButton.Size = New System.Drawing.Size(129, 66)
+        Me.AceitarButton.TabIndex = 2
+        Me.AceitarButton.Text = "Aceitar proposta"
+        Me.AceitarButton.UseVisualStyleBackColor = True
+        '
+        'VerPropostas
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.AceitarButton)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PropostasListBox)
+        Me.Name = "VerPropostas"
+        Me.Text = "VerPropostas"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PropostasListBox As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents AceitarButton As Button
+End Class
---
+++ D8 Imobiliarias/D8 Imobiliarias/VerPropostas.vb
@@ -0,0 +1,70 @@
+﻿Imports System.Data.SqlClient
+Public Class VerPropostas
+    Dim CN As SqlConnection
+    Dim dbServer = "tcp:mednat.ieeta.pt\SQLSERVER,8101"
+    Dim dbName = "p5g5"
+    Dim userName = "p5g5"
+    Dim userPass = "!DXartur2021"
+    Dim lista_total_propostas As New List(Of PropostaVB)
+    Private Sub VerPropostas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PropostasListBox.Items.Clear()
+        Dim RDR As SqlDataReader
+
+        Dim SQA As SqlDataAdapter
+
+        Dim CMD As SqlCommand
+
+        CN = New SqlConnection("data Source = " + dbServer + " ;" +
+                               "initial Catalog = " + dbName + ";" +
+                               "uid = " + userName + ";" +
+                               "password = " + userPass)
+        CMD = New SqlCommand
+        CMD.Connection = CN
+
+        CMD.CommandText = "SELECT proposta_codigo, valor, interessado_nif, imovel_codigo
+                           FROM p5g5.Proj.[proposta]"
+        CN.Open()
+        RDR = CMD.ExecuteReader
+
+        While RDR.Read
+            Dim P As New PropostaVB
+            P.proposta_codigo = RDR.Item("proposta_codigo")
+            P.valor = Convert.ToString(RDR.Item("valor"))
+            P.interessado_nif = Convert.ToString(RDR.Item("interessado_nif"))
+            P.imovel_codigo = RDR.Item("imovel_codigo")
+            For index As Integer = 0 To Opcao.lista_imoveis.Count - 1
+                If Opcao.lista_imoveis(index).imovel_codigo = P.imovel_codigo Then
+                    PropostasListBox.Items.Add(P)
+                End If
+            Next
+        End While
+        CN.Close()
+
+    End Sub
+
+    Private Sub AceitarButton_Click(sender As Object, e As EventArgs) Handles AceitarButton.Click
+        If PropostasListBox.SelectedIndex = -1 Then
+            MsgBox("Selecione uma proposta!")
+        Else
+            Dim proposta As PropostaVB
+            proposta = PropostasListBox.SelectedItem
+            Dim CMD As New SqlCommand
+            CN = New SqlConnection("data Source = " + dbServer + " ;" +
+                               "initial Catalog = " + dbName + ";" +
+                               "uid = " + userName + ";" +
+                               "password = " + userPass)
+
+            CMD.Connection = CN
+            CN.Open()
+            CMD.CommandText = "Proj.[cp_update_imovel]"
+            CMD.CommandType = CommandType.StoredProcedure
+            CMD.Parameters.Clear()
+            Dim v_imovel_codigo = proposta.imovel_codigo
+            CMD.Parameters.AddWithValue("@v_imovel_codigo", v_imovel_codigo)
+            CMD.ExecuteNonQuery()
+            CN.Close()
+            MsgBox("A proposta foi aceite com sucesso.")
+            Me.Close()
+        End If
+    End Sub
+End Class
